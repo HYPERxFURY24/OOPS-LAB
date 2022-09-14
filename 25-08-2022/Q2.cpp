60fac701396a8da08644ec7483df7ff94a38a9f4
SAHIL SINGH <107126552+HYPERxFURY24@users.noreply.github.com>
2022-09-15 00:22:06 +0530

    CSE-24

--- 25-08-2022/Q2.cpp
+++ 25-08-2022/Q2.cpp
@@ -1,7 +1,7 @@
 /*WAP to create a class employee having the name, age, id, department name and salary of the employee.
 Assign the values for 2 employees and display them.
-(1). First employees details are input by the user from keyboard at the time of creation.
-(2). Second employee details are also input by the user from keyboard where the details are dynmically
+1. First employees details are input by the user from keyboard at the time of creation.
+2. Second employee details are also input by the user from keyboard where the details are dynamically
 initialised.*/
 
 #include <iostream>
