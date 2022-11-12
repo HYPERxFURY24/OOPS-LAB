7b78a7d335effe60926beedd062501768bb03e08
SAHIL SINGH <107126552+HYPERxFURY24@users.noreply.github.com>
2022-11-12 20:21:31 +0530

    OOPS-LAB

---
+++ 03-11-2022/Q2.cpp
@@ -0,0 +1,9 @@
+/*2. WAP to create a class "length" that stores the length unit in feet and inches. Input the total 
+length in inches at the time of creation of object.
+i) Assign the length value of a respective object to system defined unit that stores the length in 
+feet & print the output.
+ii) Assign the total length unit which is stored in system defined variable to the object by using
+necessary conversion function & display the length in feet & inches.
+(Hints: Use all the constructors & destructors.)
+ */
+
---
+++ 03-11-2022/Q3.cpp
@@ -0,0 +1,6 @@
+/*3. WAP to create a class employee that stores the name, basic salary & grade of an employee.
+Overload the new operator to allocate memory and then stores the value. Also overload the delete
+operator to deallocate the memory. Also add the user-defined constructors and destructors in
+the program. Using the necessary member function, assign the name of employee using system-defined
+variable & display it.*/
+
